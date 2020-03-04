1ae6aa80009df240924fdf22d2b65f15bd85621c
EdwinMLara <emlara35@gmail.com>
2020-03-03 21:45:41 -0600

    configurando el archivo de tabs donde se le agrega la clase active a la pesteña seleccionada

--- tabs.php
+++ tabs.php
@@ -4,7 +4,9 @@
         $sql_total_paginas = "SELECT * FROM area";
         $sql_result = mysqli_query($con,$sql_total_paginas);
         $bandera = 0;
+        $count = 0;
          while($area = mysqli_fetch_array($sql_result)){
+             $count += 1;
             if(!$bandera){ ?>
                 <ul class="nav nav-tabs">
             <?php
@@ -14,7 +16,13 @@
             $nombre_area = $area["nombre_area"]; ?>
                 <li class="nav-item">
                     <input id="<?php echo $nombre_area; ?>" type="hidden" value="<?php echo $aux_page; ?>">
-                    <a class="nav-link active" href="<?php echo 'index.php?area='.$aux_page; ?>"><?php echo $nombre_area; ?></a>
+                    <a class="nav-link <?php if(isset($_GET["area"])){
+                    if($aux_page == $_GET["area"])
+                        echo "active";
+                    }else{
+                        if($count == 1)
+                            echo "active";
+                    } ?>" href="<?php echo 'index.php?area='.$aux_page; ?>"><?php echo $nombre_area; ?></a>
                 </li>
             <?php
             $bandera++;
